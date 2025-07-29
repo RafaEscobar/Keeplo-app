38f69ca2541bf7acd415b279fda7b868aa1bb640
Rafael Escobar <Rafael.EG.code@gmail.com>
2025-07-28 21:38:37 -0600

    Estructura de registro

--- lib/screens/main/register_screen.dart
+++ lib/screens/main/register_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:keeplo/theme/app_theme.dart';
+import 'package:keeplo/utils/responsive.dart';
+import 'package:keeplo/widgets/main/footer_main.dart';
+import 'package:keeplo/widgets/main/header_main.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -6,6 +11,75 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool isHorizontal = Responsive.isHorizontalTablet(context);
+
+    Widget headerMain = HeaderMain(
+      imageUrl: "assets/pictures/register.png",
+      title: "Registrarme",
+    );
+
+    Widget footerMain = FooterMain(
+      btnText: 'Crear cuenta',
+      callback: () {},
+      isLogin: false,
+    );
+
+    Widget horizontalTabletBody = Row(
+      children: [
+        Expanded(child: headerMain),
+        const SizedBox(width: 60),
+        Expanded(
+          child: Column(
+            children: [
+              //LoginForm(),
+              const SizedBox(height: 20),
+              Spacer(),
+              footerMain
+            ],
+          ),
+        ),
+      ],
+    );
+
+    Widget regularBody = Column(
+      children: [
+        headerMain,
+        SizedBox(height: 20.h),
+        //LoginForm(),
+        const Spacer(),
+        footerMain
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: EdgeInsets.only(left: 20),
+          child: GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Icon(Icons.arrow_back_ios, color: Colors.white, size: 30,),
+          ),
+        ),
+      ),
+      backgroundColor: AppTheme.primary,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: isHorizontal ? horizontalTabletBody : regularBody
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/main/footer_main.dart
+++ lib/widgets/main/footer_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:keeplo/screens/main/register_screen.dart';
 import 'package:keeplo/theme/app_theme.dart';
 import 'package:keeplo/utils/responsive.dart';
 import 'package:keeplo/widgets/simple_button.dart';
@@ -16,28 +18,29 @@ class FooterMain extends StatelessWidget {
     return Column(
       children: [
         SimpleButton(text: btnText, callback: callback),
+        SizedBox(height: 16.h),
         Visibility(
           visible: isLogin,
           child: Column(
             children: [
-              SizedBox(height: 10.h),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
                     "¿Aún no tienes una cuenta?",
-                    style: TextStyle(color: Colors.white, fontSize: isHorizontal ? 22 : Responsive.resize(size: 16.sp, reduction: .9)),
+                    style: TextStyle(color: Colors.white, fontSize: isHorizontal ? 22 : Responsive.resize(size: 18.sp, reduction: .9)),
                   ),
                   const SizedBox(width: 6),
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () => context.goNamed(RegisterScreen.routeName),
                     child: Text(
                       "Crear una",
-                      style: TextStyle(color: AppTheme.error, fontSize: isHorizontal ? 22 : Responsive.resize(size: 16.sp, reduction: .9), fontWeight: FontWeight.bold),
+                      style: TextStyle(color: AppTheme.error, fontSize: isHorizontal ? 22 : Responsive.resize(size: 18.sp, reduction: .9), fontWeight: FontWeight.bold),
                     ),
                   ),
                 ],
               ),
+              SizedBox(height: 4),
             ],
           )
         )
